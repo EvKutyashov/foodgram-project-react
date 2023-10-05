4481f2d1a0211d77d3ec33a08bee8091600c2b3a
Кутяшов Евгений <evgeniykutyashov@gmail.com>
2023-10-05 21:16:53 +0300

    fix`s

--- backend/recipes/validators.py
+++ backend/recipes/validators.py
@@ -4,7 +4,7 @@ from django.core.exceptions import ValidationError
 def validate_file_size(value):
     filesize = value.size
 
-    if filesize > 10485760:
+    if filesize > 1048576:
         raise ValidationError(
             "Вы не можете загрузить файл размером больше 1Mб")
     else:
